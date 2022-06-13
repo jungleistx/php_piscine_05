cfdb371337c3def2d5ce712166e7039985aefe3c
Roope Vuorenlehto <rvuorenl@c1r3p6.hive.fi>
2022-06-13 17:57:55 +0300

    ex01, fix group quotes

--- ex01/ex01.sql
+++ ex01/ex01.sql
@@ -1,6 +1,6 @@
 CREATE TABLE ft_table (
 	id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-	login VARCHAR(255) default 'toto' NOT NULL,
-	group ENUM('staff', 'student', 'other') NOT NULL,
+	login VARCHAR(8) default 'toto' NOT NULL,
+	`group` ENUM('staff', 'student', 'other') NOT NULL,
 	creation_date DATE NOT NULL
 );
