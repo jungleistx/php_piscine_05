9f5db2f8dd71037b2b6752f15c6918d02098b438
Roope Vuorenlehto <rvuorenl@c1r3p6.hive.fi>
2022-06-14 20:38:31 +0300

    ex16, add parentheses to OR

--- ex16/ex16.sql
+++ ex16/ex16.sql
@@ -1,3 +1,3 @@
 SELECT count(*) AS 'movies' FROM member_history
 WHERE DATE(date) BETWEEN '2006-10-30' AND '2007-07-27'
-OR MONTH(date) = 12 AND DAY(date) = 24;
+OR (MONTH(date) = 12 AND DAY(date) = 24);
