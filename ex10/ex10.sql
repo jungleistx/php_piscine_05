438914d2a26d4297af3a5802080e74346e8802b8
Roope Vuorenlehto <rvuorenl@c1r3p6.hive.fi>
2022-06-14 20:03:05 +0300

    ex10, fix JOIN type

--- ex10/ex10.sql
+++ ex10/ex10.sql
@@ -1,4 +1,4 @@
 SELECT title AS 'Title', summary AS 'Summary', prod_year FROM film
-LEFT JOIN genre ON film.id_genre = genre.id_genre
+INNER JOIN genre ON film.id_genre = genre.id_genre
 WHERE genre.name = 'erotic'
-ORDER BY film.prod_year DESC;
+ORDER BY prod_year DESC;
