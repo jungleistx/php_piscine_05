aac32307b97a4f45604c5fd57911b43c5120c946
Roope Vuorenlehto <rvuorenl@c1r3p6.hive.fi>
2022-06-14 19:38:57 +0300

    ex02, fix dates with '-'

--- ex02/ex02.sql
+++ ex02/ex02.sql
@@ -1,6 +1,6 @@
 INSERT INTO ft_table (login, `group`, creation_date)
-VALUES	('loki', 'staff', '20130501'),
-('scadoux', 'student', '20140101'),
-('chap', 'staff', '20110427'),
-('bambou', 'staff', '20140301'),
-('fantomet', 'staff', '20100403');
+VALUES	('loki', 'staff', '2013-05-01'),
+('scadoux', 'student', '2014-01-01'),
+('chap', 'staff', '2011-04-27'),
+('bambou', 'staff', '2014-03-01'),
+('fantomet', 'staff', '2010-04-03');
