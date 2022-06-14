0792696507a3db050aa646f4afc8c8903106bc5f
Roope Vuorenlehto <rvuorenl@c1r3p6.hive.fi>
2022-06-14 19:56:01 +0300

    ex08, change extract to year

--- ex08/ex08.sql
+++ ex08/ex08.sql
@@ -1,3 +1,3 @@
 SELECT last_name, first_name, DATE(birthdate) AS birthdate FROM user_card
-WHERE EXTRACT(YEAR FROM birthdate) = 1989
-ORDER BY last_name;
+WHERE YEAR(birthdate) = '1989'
+ORDER BY birthdate;
